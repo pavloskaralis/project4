4a3ce06647155bac801d5e5be6b350c25d8686ff
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-02-01 22:14:53 -0500

    progres

--- polling_frontend/src/App.js
+++ polling_frontend/src/App.js
@@ -1,66 +1,106 @@
 import React, { useEffect, useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Chart from 'chart.js'
-import history from '../../history'
+import history from './history'
 import axios from 'axios'
 import './App.css'
 
 function App() {
-
   const [data, updateData] = useState();
-  const [month, updateMonth] = useState('');
-  const [candidates, updateCandidates] = useState([]);
+  const [monthButtons, updateMonthButtons] = useState(['']);
+  const [chart, updateChart] = useState(null);
+  const [month, updateMonth] = useState('all');
+  const [candidates, updateCandidates] = useState(['Bernie Sanders','Joe Biden']);
 
- useEffect( () => { 
   const getData = () => {
-    
-    axios.get('http://localhost:3001/path')
-    .then(({data}) => this.prepareData(data))
-    .then(chartData => this.createChart(chartData))
+    axios.get('http://localhost:3001/candidates')
+    .then(({data}) => prepareData(data))
+    .then(chartData => createChart(chartData))
   }
 
- 
   const prepareData = (data) => {
-
-    const labels = month ? candidates.slice() : ['January 2019']
-
-    const chartData = { 
-      labels: [],
-      datasets: [
-
-        {
-          label: 'label',
-          data: []
-        }
-      ]
-    }
+    //dynamic month filter
+    const months = Object.keys(data[0]).filter(key => ['id','name','created_at','updated_at'].indexOf(key) === -1);
+    //pass months to state for dynamic render
+    updateMonthButtons(['all',...months]);
+    //passed to Chart datasets property
+    const datasetsArr = [];
+    //render selected candidates only
+    candidates.forEach((candidate) => {
+      //find candidate in component data
+      const index = data.findIndex(obj => candidate === obj.name)
+      //passed to data property of dataset object
+      const candidateData = [];
+      //data based on month conditional
+      if (month !== 'all') {
+        candidateData.push(data[index][month]);
+      } else {
+        for (let i = 0; i < months.length; i ++) candidateData.push(data[index][months[i]]);
+      }
+      //bar color based on candidate    
+      const color = {
+        'Joe Biden': 'red',
+        'Bernie Sanders': 'blue',
+        'Elizabeth Warren': 'green',
+        'Pete Buttigieg': 'orange',
+        'Andrew Yang': 'gold'
+      }[candidate]
+      
+      datasetsArr.push({label: candidate, data: candidateData, backgroundColor: color})
+    })
   
-    data.x_prop.forEach(x_prop => {
-      chartData.labels.push(x_prop.id)
-      chartData.datasets[0].data.push(x_prop.value)
-    })  
+    const chartData = { 
+      labels: month !== 'all' ? [month] : months,
+      datasets: datasetsArr
+    }
   
     return chartData
   }
 
   const createChart = (data) => {
-    const ctx = document.querySelector('#container')
+    const ctx = document.querySelector('#chart')
     const tempsChart =  new Chart(ctx, {
-      type: month ? 'bar' : 'line',
-      data: data
+      type: month !== 'all' ? 'bar' : 'line',
+      data: data,
+      options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                } 
+            }]
+        }
+      }
     })
+    //store chart to destroy on remount
+    updateChart(tempsChart);
   }
- }, [])  
-  
- 
+
+  //componentDidMount hook; remount on month and candidate changes only
+  useEffect(getData, [candidates,month]);
+
   return (
     <>
       <nav>
         <h1>2020 Democratic Polling Averages</h1>
       </nav>
-
+      <canvas id="chart" ></canvas>
+      <div className='duration'>
+        {monthButtons.map(month => {
+          return (
+            <button key={month} onClick={()=>{
+              chart.destroy();
+              updateMonth(month);
+            }}>{month}</button>
+          )
+        })}
+      </div>
+      <div className='candidates'>
+        
+      </div>
     </>
   )
+  
 }
 
 export default App
--- polling_frontend/src/index.js
+++ polling_frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App/App'
+import App from './App'
 import {Router} from 'react-router-dom'
 import history from './history.js'
 
